7544b7c5d40f339b7f7d74a8018d07232468999c
Rvt129 <baothuvoal@gmail.com>
2025-06-21 19:53:54 +0700

    fix

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,6 @@ const path = require("path");
 const connectDB = require("./config/db");
 const User = require("./models/User");
 const dotenv = require("dotenv");
-const express = require("express");
 const compression = require("compression");
 
 // Bật nén Gzip/Brotli
